832ab306300c3944f0b0bf3fbc106cdcbde811df
kashif naseem <56063644+kashifnaseem50@users.noreply.github.com>
2021-12-02 13:37:15 +0500

    Update master-and-nodes.sh

--- master-and-nodes.sh
+++ master-and-nodes.sh
@@ -10,7 +10,7 @@ docker --version
 systemctl start docker
 systemctl enable docker
 
-sudo curl -s https://packages.cloud.google.com/apt... | sudo apt-key add 
+sudo curl -s https://www.youtube.com/redirect?event=video_description&redir_token=QUFFLUhqbWlvWkZOX1lFVnBVZkYzWXJlaEJuYW1wUUJpUXxBQ3Jtc0ttelMxZ3R2Vm5SeFEtaHZuRVFoWktEUWQtWFRUWE1hUnVCUkhoT25tY0tKTkNIandtd0x4T0pvcHNfZmxfVUN1Yk9BSXUzRnpxZy1Cc05zbnlraV9BbEF6TV90cWxnVzFGWWRzNUY4NzlRVjlIWjhOcw&q=https%3A%2F%2Fpackages.cloud.google.com%2Fapt%2Fdoc%2Fapt-key.gpg | sudo apt-key add 
 
 
 nano /etc/apt/sources.list.d/kubernetes.list
